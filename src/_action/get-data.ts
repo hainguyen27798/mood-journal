5daa321596de40f283b22dff4d8cb275146d8f35
harry Nguyen <hainguyen27798@gmail.com>
2024-11-11 01:04:19 +0700

    update read csv

--- src/_action/get-data.ts
+++ src/_action/get-data.ts
@@ -1,27 +1,35 @@
-import { createReadStream } from 'node:fs';
-
-import { parse } from 'csv-parse';
 import dayjs from 'dayjs';
 import _ from 'lodash';
+import papa from 'papaparse';
 
 import type { TRecord } from '@/types';
 
 export const getData = async (): Promise<TRecord[]> => {
-  return new Promise((resolve) => {
-    const result: TRecord[] = [];
-    createReadStream(process.cwd() + '/mock/data.csv')
-      .pipe(parse({ delimiter: ',', columns: true }))
-      .on('data', (row) => {
-        result.push({
-          date: dayjs(row.date).startOf('date').valueOf(),
-          distance: _.toNumber(row.distance),
-          pace: _.toNumber(row.pace),
-          heartRate: _.toNumber(row.heart_rate),
-          calories: _.toNumber(row.calories),
-        });
-      })
-      .on('end', () => {
-        resolve(result);
-      });
+  const result: TRecord[] = [];
+  const url = process.env.NEXT_PUBLIC_MOCK_DATA_URL!;
+  const csv = await fetch(url, {
+    method: 'get',
+    headers: {
+      'content-type': 'text/csv;charset=UTF-8',
+    },
   });
+
+  if (csv.ok) {
+    const data = papa.parse<any>(await csv.text(), {
+      header: true,
+      delimiter: ',',
+    }).data;
+
+    _.forEach(data, (row) => {
+      result.push({
+        date: dayjs(row.date).startOf('date').valueOf(),
+        distance: _.toNumber(row.distance),
+        pace: _.toNumber(row.pace),
+        heartRate: _.toNumber(row.heart_rate),
+        calories: _.toNumber(row.calories),
+      });
+    });
+  }
+
+  return result;
 };
